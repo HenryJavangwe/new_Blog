80020eef2806ba52a18bf0bc3adb4a30bf515b24
Henry Javangwe <henrysparks1@gmail.com>
2020-05-18 14:57:20 +0200

    created an array with an  object articles whhere i created a placeholder article that i'll use later. created a forEach function to iterate through that array and access and briing out the information inside to be displayed on th eindex.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const articles = [{
+    title:'Article1',
+    author:'Baki',
+    body:'This is the article body',
+    image: 'https://images.unsplash.com/photo-1553524787-74454297efc2?ixlib=rb-1.2.1&auto=format&fit=crop&w=1191&q=80',
+    default: new Date()
+  }]
+  res.render('index', { articles: articles });
 });
 
 module.exports = router;
