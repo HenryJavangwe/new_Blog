84b1796d054c14f95b42498c1b5c7495af0e4e48
Henry Javangwe <henrysparks1@gmail.com>
2020-05-18 17:46:23 +0200

    after creating the atrticle schema and exporting the model, we require it inside the new.js router and in here, inside the router.post, we accessed the form fields using the req.body.params -- this is made possible when we tell the --> app.use(express.urlencoded({ extended: false })).//this is an async function so we tell use the keywords async and then await and this will save our new article.

--- routes/new.js
+++ routes/new.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Article = require('./../models/article')
 
 // Grabbing the create new article page
 
@@ -9,8 +10,16 @@ router.get('/', function(req, res, next) {
 
 //whenever we submit the form it'll call this router.post which is at this '/' 
 
-router.post('/', function(req, res, next) {
-      res.send('Form Submitted Successfully!!');
+router.post('/', async function(req, res, next) {
+    let article = new Article({
+        //access the form fields using the req.body.params -- this is made possible when we tell the --> app.use(express.urlencoded({ extended: false })).
+        title: req.body.title,
+        author: req.body.author,
+        body: req.body.body,
+        image: req.body.image,
+        createdAt: req.body.createdAt
+    })
+    await article.save()//this is an async function so we tell use the keywords async and then await and this will save our new article.
   });
 
 // To be able to get a hold of this route out side this module, export it like below.
