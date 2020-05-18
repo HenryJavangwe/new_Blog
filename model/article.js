97ecdb855c999cb78cbc07cde3f430b10c57bc9a
Henry Javangwe <henrysparks1@gmail.com>
2020-05-18 17:34:26 +0200

    Created an article Schema and then exported it out so that we can use it in any of of modules.

--- model/article.js
+++ model/article.js
@@ -1,1 +1,25 @@
 const mongoose = require('mongoose')
+
+const articleSchema = new mongoose.Schema({
+    title : {
+        type    : String,
+        required: true
+    },
+    author : {
+        type    : String,
+        required: true
+    },
+    body : {
+        type    : String,
+        required: true
+    },
+    image : {
+        type    : String,
+    },
+    createdAt : {
+        type    : Date,
+        default : new Date()
+    }
+})
+
+module.exports = mongoose.model('Article', articleSchema)
