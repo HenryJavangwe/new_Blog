9d81fd1d22ee9d7399d4dd41d1c8d8dbf1ffd631
Henry Javangwe <henrysparks1@gmail.com>
2020-05-20 18:47:46 +0200

    created two buttons on the view page, the Home button and the Edit button. Inside the index.js  route where we had placeholder articles, I removed those and created a new articles variable then set it to our Article model and used a method .find() to grab and hold all the articles we created and display them on our index. Then furrther used the .sort function to sort the articles in a descending order.

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const Article = require('../models/article')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const articles = [{
-    title:'Article1',
-    author:'Baki',
-    body:'This is the article body',
-    image: 'https://images.unsplash.com/photo-1553524787-74454297efc2?ixlib=rb-1.2.1&auto=format&fit=crop&w=1191&q=80',
-    createdAt: new Date()
-  },
-  {
-    title:'Article 2',
-    author:'Baki Sparks',
-    body:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    image: 'https://images.unsplash.com/photo-1552561040-3ff08bed3eba?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-    createdAt: new Date()
-  }]
+router.get('/', async function(req, res, next) {
+  const articles = await Article.find().sort({
+    createdAt: 'desc' //used .sort() function to sort the articles in descending order so that the latest articles will appear at the top and the oldest at the bottom.
+  }); // This will grab all our articles from the database -> it's an async function
+
   res.render('index', { articles: articles });
 });
 
