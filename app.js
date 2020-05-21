8604991e63e6d2a276e71aa140aff6ce88b78fc4
Henry Javangwe <henrysparks1@gmail.com>
2020-05-21 12:29:38 +0200

    required the methodOverride in the app.js. To prevent the unhandled errors in the console, I made all the input fields of the form to be required. created the card layout of the articles that will Displayed on the index page. created the delete button with a link to the delete route and a method override of the default form post form (/create/<%=article.id%>?_method=DELETE). Styled the card using bootstrap by giving the card-overlay content a dark back ground and then making the background a bit transparent.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')// require mongoose for database
 
 // connecting to our Data base
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'))// now if we call _method delete/put we will be able to 
 
 // Register Routers
 app.use('/', indexRouter);
